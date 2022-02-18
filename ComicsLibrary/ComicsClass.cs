cfdb9c5c996ddd1df66081d322bda9672e65b503
SWwAaG <99506563+SWwAaG@users.noreply.github.com>
2022-02-18 15:34:32 +0300

    =
    =

---
+++ ComicsLibrary/ComicsClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ComicsLibrary
+{
+    public class Comics
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Img { get; set; }
+    }
+}
